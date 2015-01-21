a2db8c621cf689ef92da8587366f48291f2979b1
MonicaFiguera <monica.yuncosa@gmail.com>
2015-01-20 23:47:08 -0430

    CasosDePrueba

--- Casodeprueba.py
+++ Casodeprueba.py
@@ -1,7 +1,8 @@
 '''
 Created on Jan 18, 2015
 
-@author: maria-esthervidal
+@author: Monica Figuera  11-10328
+	 Enrique Iglesias 11-10477
 Se considera la tarifa diurna desde las 6:00 hasta 18:00
 Se considera la tarifa nocturna desde las 18:01 hasta 05:59
 '''
@@ -17,97 +18,97 @@ class TestFecha(unittest.TestCase):
         
         formato = '%d/%m/%Y %H:%M'
         
-        #Frontera
+        #FronteraMinReservaValida
         fi = datetime.strptime("01/01/2015 16:30", formato)
         ff = datetime.strptime("01/01/2015 16:45", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
         self.assertEquals(tarifa, 50)
         
-        #Frontera
+        #FronteraMinReservaValida
         fi = datetime.strptime("01/01/2015 04:30", formato)
         ff = datetime.strptime("01/01/2015 04:45", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
         self.assertEquals(tarifa, 100)
         
-        #Frontera
+        #FronteraMaxReservaValida
         fi = datetime.strptime("01/01/2015 05:00", formato)
         ff = datetime.strptime("04/01/2015 05:00", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
         self.assertEquals(tarifa, 5400)
         
-        #Esquina y Frontera
+        #Esquina y Frontera minimo tiempo de reserva valida un min antes de cambio de tasa (de nocturna a diurna)
         fi = datetime.strptime("01/01/2015 05:45", formato)
         ff = datetime.strptime("01/01/2015 6:00", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
         self.assertEquals(tarifa, 100)
         
-        #Esquina y Frontera
+        #Esquina y Frontera minimo tiempo de reserva valida un min antes de cambio de tasa (de diurna a nocturna)
         fi = datetime.strptime("01/01/2015 17:45", formato)
         ff = datetime.strptime("01/01/2015 18:00", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
         self.assertEquals(tarifa, 50)
         
-        #Esquina y Frontera
+        #Esquina y Frontera minimo tiempo reserva valida en cambio de tasa (diurna a nocturna)
         fi = datetime.strptime("01/01/2015 17:46", formato)
         ff = datetime.strptime("01/01/2015 18:01", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
         self.assertEquals(tarifa, 100)
         
-        #Esquina 
+        #Esquina desde el inicio de la tarifa diurna hasta el final
         fi = datetime.strptime("01/01/2015 06:00", formato)
         ff = datetime.strptime("01/01/2015 18:00", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
         self.assertEquals(tarifa, 600)
         
-        #Esquina
+        #Esquina desde el inicio de la tarifa diurna hasta su proximo inicio del dia siguiente
         fi = datetime.strptime("01/01/2015 06:00", formato)
         ff = datetime.strptime("02/01/2015 06:00", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
         self.assertEquals(tarifa, 1800)
         
-        #Esquina
+        #Esquina cambio de tasa de diurna a nocturna.
         fi = datetime.strptime("01/01/2015 17:00", formato)
         ff = datetime.strptime("01/01/2015 18:01", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
         self.assertEquals(tarifa, 150)
         
-        #Esquina
+        #Esquina un minuto antes del cambio de tasa de diurna a nocturna
         fi = datetime.strptime("01/01/2015 17:00", formato)
         ff = datetime.strptime("01/01/2015 18:00", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
         self.assertEquals(tarifa, 50)
         
-        #Esquina
+        #Esquina inicio de la tarifa nocturna hasta su final
         fi = datetime.strptime("01/01/2015 18:01", formato)
         ff = datetime.strptime("02/01/2015 05:59", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
         self.assertEquals(tarifa, 1200)
         
-        #Esquina y Frontera
+        #Esquina y Frontera maximo tiempo posible de reservacion (72h) iniciando desde las 6:00pm
         fi = datetime.strptime("01/01/2015 18:00", formato)
         ff = datetime.strptime("04/01/2015 18:00", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
         self.assertEquals(tarifa, 5400)
         
-        #Esquina y Frontera
+        #Esquina y Frontera maximo tiempo posible de reservacion (72h) iniciando desde las 6:00am
         fi = datetime.strptime("01/01/2015 06:00", formato)
         ff = datetime.strptime("04/01/2015 06:00", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
         self.assertEquals(tarifa, 5400)
         
-        #Esquina
+        #Esquina cambio continuo de tasas por 3 días
         fi = datetime.strptime("01/01/2015 18:00", formato)
         ff = datetime.strptime("04/01/2015 06:00", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
         self.assertEquals(tarifa, 4800)
         
-        #Frontera
+        #Frontera tiempo de reservacion invalido (menor que el minimo)
         fi = datetime.strptime("01/01/2015 17:00", formato)
         ff = datetime.strptime("01/01/2015 17:10", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
         self.assertEquals(tarifa, 0)
         
-        #Frontera
+        #Frontera tiempo de reservacion invalido (mayor que el maximo)
         fi = datetime.strptime("01/01/2015 18:00", formato)
         ff = datetime.strptime("05/01/2015 18:00", formato)
         tarifa = CalcularTarifa(fi, ff, 50, 100)
